3534f92fd018a10355af9fca93b0db9cdcbdefc7
holyndimuli97 <168868162+holyndimuli97@users.noreply.github.com>
2024-06-20 15:45:23 +0300

    initial

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { SignedIn, UserButton } from '@clerk/nextjs';
-
 import MobileNav from './MobileNav';
+import {autoPlacement} from "@floating-ui/dom";
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/icons/logo.svg"
-          width={32}
-          height={32}
-          alt="yoom logo"
-          className="max-sm:size-10"
-        />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          YOOM
+         <div className="logo-container">
+          <Image
+            src="/icons/logo.png"
+            layout="intrinsic"
+            width={75} // Double the original width (example)
+            height={32} // Double the original height (example)
+            alt="yoom logo"
+            className="logo-image"
+          />
+        </div>
+        <p className="text-[2px] font-extrabold text-white max-sm:hidden">
+          .
         </p>
       </Link>
       <div className="flex-between gap-5">
